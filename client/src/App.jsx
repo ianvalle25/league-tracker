d66bbf056f860c5dbf6c2c74743bd196f243c9eb
Sawble <valleian25@gmail.com>
2025-07-13 22:00:16 -0700

    Added Player Search
    User is now able to search up name using the game name and unique id to dispaly info from Flask to React

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+  const [riotId, setriotId] = useState("");
+
+  const handleUserSubmit = async (e) => {
+    e.preventDefault();
+
+   try {
+     const res = await fetch("http://localhost:5000/summoner", {
+        method: "POST",
+       headers: { "Content-Type": "application/json" },
+       body: JSON.stringify({ riotId: riotId })
+      });
+      const data = await res.json();
+      setData(data);
+    } catch (err) {
+     console.error("Error:", err);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form onSubmit={handleUserSubmit}>
+        <input
+          placeholder="Username + ID"
+          value={riotId}
+          onChange={(e) => setriotId(e.target.value)}
+        >
+        </input>
+        <button type="submit">Search</button>
+      </form>
+      <h1>Tilted</h1>
+      {data ? (
+  <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+       <p>Enter a Riot ID and search.</p>
+      )}
+       </div>
+   );
 }
 
-export default App
+export default App;
---
+++ server/app.py
@@ -0,0 +1,78 @@
+from flask import Flask, jsonify,request
+import requests
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+RIOT_API_KEY = ""
+
+
+@app.route("/summoner/<game_name>/<tag_line>")
+def get_summoner_info(game_name, tag_line):
+    try:
+        headers = {"X-Riot-Token": RIOT_API_KEY}
+        
+        url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}"
+        account_res = requests.get(url, headers=headers)
+        if account_res.status_code != 200:
+            return jsonify({
+                "error": "Riot ID not found",
+                "status": account_res.status_code,
+                "message": account_res.json().get("status", {}).get("message", "")
+            }), account_res.status_code
+
+
+        account_data = account_res.json()
+        puuid = account_data["puuid"]
+
+        url2 = f"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}"
+        summoner_res = requests.get(url2, headers=headers)
+        summoner_data = summoner_res.json()
+
+        solo_rank = {
+            "tier": "UNRANKED",
+            "rank": "",
+            "leaguePoints": 0
+        }
+
+        url3 = f"https://na1.api.riotgames.com/lol/league/v4/entries/by-puuid/{puuid}"
+        summoner_entry_res = requests.get(url3, headers=headers)
+        summoner_entry_data = summoner_entry_res.json()
+
+        for entry in summoner_entry_data:
+            if entry["queueType"] == "RANKED_SOLO_5x5":
+                solo_rank = {
+                    "tier": entry["tier"],
+                    "rank": entry["rank"],
+                    "leaguePoints": entry["leaguePoints"]
+                 }
+
+        return jsonify({
+            "gameName": account_data["gameName"],
+            "tagLine" : account_data["tagLine"],
+            "rank" : solo_rank["rank"],
+            "leaguePoints" : solo_rank["leaguePoints"],
+            "tier" : solo_rank["tier"],
+            "summonerLevel" : summoner_data["summonerLevel"]
+
+
+            })
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+@app.route("/summoner", methods=["POST"])
+def get_summoner_from_post():
+      try:
+        data = request.get_json()
+        riot_id = data.get("riotId")
+
+        if not riot_id or "#" not in riot_id:
+            return jsonify({"error": "Invalid Riot ID format"}), 400
+
+        game_name, tag_line = riot_id.split("#")
+        return get_summoner_info(game_name=game_name, tag_line=tag_line)
+      except Exception as e:
+        return jsonify({"error": str(e)}), 500
